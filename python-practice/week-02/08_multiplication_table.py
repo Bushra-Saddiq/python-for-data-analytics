bd7e85a2b91518d1dee25658e86455bf92a8d8d9
Bushra Saddiq <saddiqbushra69@gmail.com>
2026-09-01 16:59:23 +0500

    Add files via upload

---
+++ python-practice/week-02/08_multiplication_table.py
@@ -0,0 +1,7 @@
+# Task: Using a nested for loop, print a multiplication table for
+# numbers 1 to 3.
+# Topics: nested loops
+
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(f"{i} * {j} = {i*j}")
